5ea34711056a49c252a54b1ae0f5de9f4cb60a70
Miki Pavlov <miki.pavlov@namics.com>
2021-03-25 11:31:10 +0100

    add new changes

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Button Component", () => {
+  it("should render the button with no errors", () => {
+    render(<Button />);
+
+    screen.getByRole("button");
+  });
+});
